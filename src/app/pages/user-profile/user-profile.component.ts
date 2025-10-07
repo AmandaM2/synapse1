9d706126f90ad4ad930d120c44511c84b301d0b5
AmandaM2 <aaamnd112@gmail.com>
2025-10-07 11:21:24 -0300

    Perfis completos!

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,28 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { User } from '../../models/user.model';
 import { AuthService } from '../../services/auth.service';
+import { ProjectService } from '../../services/project.service';
+import { Project } from '../../models/project.model';
+
+
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
 export class UserProfileComponent implements OnInit {
   user: User | undefined;
+  userProjects: Project[] = [];
 
   constructor(
     private route: ActivatedRoute,
-    private authService: AuthService
+    private authService: AuthService,
+    private projectService: ProjectService 
   ) {}
 
   ngOnInit(): void {
     const userId = this.route.snapshot.paramMap.get('id');
     if (userId) {
       this.user = this.authService.getUserById(userId);
+      // VAI BUSCAR OS PROJETOS DESTE UTILIZADOR
+      this.userProjects = this.projectService.getProjectsByAuthorId(userId);
     }
   }
 }
